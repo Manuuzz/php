3578e19cc590e4067ac28c4b8c3ce8bbcccba401
Manuuzz <manu60120@gmail.com>
2021-10-19 14:29:30 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Page d'acceuil</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href=''>
+</head>
+<body>
+    <?php
+    
+
+    ?>
+        <a HREF="exo1.php">Exo1.php</a>
+        <p><a HREF="http://192.168.64.116/">Retour</a></p>
+
+
+
+</body>
+</html>
